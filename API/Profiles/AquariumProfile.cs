ed009ee3cb73ae0950d4282d9198f0ad3e79d59c
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2023-02-21 10:05:50 +0100

    Add automapper

---
+++ API/Profiles/AquariumProfile.cs
@@ -0,0 +1,12 @@
+﻿using Models.ViewModels.Aquarium;
+
+namespace Aqua_Sharp_Backend.Profiles
+{
+    public class AquariumProfile: Profile
+    {
+        public AquariumProfile()
+        {
+            CreateMap<Aquarium, CreateAquariumViewModel>().ReverseMap().ForAllMembers(x => x.Ignore());
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 global using Microsoft.EntityFrameworkCore;
 global using Models.Models;
+global using AutoMapper;
 
 using Aqua_Sharp_Backend.Services.AquariumService;
 using Aqua_Sharp_Backend.Contexts;
@@ -9,8 +10,9 @@ using Aqua_Sharp_Backend.Services.MeasurmentService;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
