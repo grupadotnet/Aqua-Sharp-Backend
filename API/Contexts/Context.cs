2a70ae3c032d59d491c6f99719c1b6106a502b2c
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-02-24 13:02:35 +0100

    Refactored Context

--- API/Contexts/Context.cs
+++ API/Contexts/Context.cs
@@ -5,32 +5,6 @@ namespace Aqua_Sharp_Backend.Contexts
 {
     public class Context : DbContext
     {
-        public Context(DbContextOptions<Context> options): base(options)
-        {
-
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-
-            modelBuilder.Entity<Config>()
-                .HasData(new Config
-                {
-                    Id = 1,
-                    Password = "password",
-                    FirstRun = true,
-                    Question = "",
-                    Answer = "",
-                });
-
-
-
-        }
-
         #region Entities
 
         public DbSet<Config> Config { get; set; }
@@ -39,5 +13,25 @@ namespace Aqua_Sharp_Backend.Contexts
         public DbSet<Measurement> Measurements{ get; set; }
 
         #endregion
+        
+        public Context(DbContextOptions<Context> options): base(options) { }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Config>()
+                .HasData(new Config
+                {
+                    Id = 1,
+                    Password = "password",
+                    FirstRun = true,
+                    Question = "",
+                    Answer = ""
+                });
+        }
     }
 }
