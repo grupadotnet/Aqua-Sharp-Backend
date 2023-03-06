88cacc7b833db6e017e0cade14ac85955b49cfc6
Bartosz Bizoń <bartosz.bizon17@gmai.com>
2023-03-06 11:42:10 +0100

    Zroniony szkielet AddDevice

--- API/Controllers/DeviceController.cs
+++ API/Controllers/DeviceController.cs
@@ -1,6 +1,8 @@
-﻿using Aqua_Sharp_Backend.Interfaces;
+﻿using System.Net.Sockets;
+using Aqua_Sharp_Backend.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models.ViewModels.Device;
 
 namespace Aqua_Sharp_Backend.Controllers
 {
@@ -13,6 +15,14 @@ namespace Aqua_Sharp_Backend.Controllers
         public DeviceController(IDeviceService deviceService)
         {
             _deviceService = deviceService; 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDevice([FromBody] CreateDeviceViewModel createDeviceViewModel)
+        {
+            var device = await _deviceService.Add(createDeviceViewModel);
+
+            return Ok(device);
         }
     }
 }
--- API/Profiles/DeviceProfile.cs
+++ API/Profiles/DeviceProfile.cs
@@ -6,8 +6,12 @@ namespace Aqua_Sharp_Backend.Profiles
     {
         public DeviceProfile()
         {
-            CreateMap<Device, CreateDeviceViewModel>().ReverseMap().ForAllMembers(x => x.Ignore());
-            CreateMap<Device, DeviceViewModel>().ReverseMap().ForAllMembers(x => x.Ignore());
+            CreateMap<Device, CreateDeviceViewModel>()
+                .ReverseMap()
+                .ForAllMembers(x => x.Ignore());
+            CreateMap<Device, DeviceViewModel>()
+                .ReverseMap()
+                .ForAllMembers(x => x.Ignore());
         }
     }
 }
