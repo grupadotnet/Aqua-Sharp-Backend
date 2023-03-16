acfe6aadfe16c1d0d03b3fedd7408d10bc4647cb
Bartosz Bizoń <bartosz.bizon17@gmai.com>
2023-03-17 00:02:30 +0100

    Fixed one-to-one relation between Aquarium and Device

--- Models/Entities/Device.cs
+++ Models/Entities/Device.cs
@@ -3,10 +3,10 @@
     public class Device
     {
         public int DeviceId { get; set; }
-        public int AquariumId { get; set; }
         public uint MeasurementFrequency { get; set; }
         public bool ManualMode { get; set; } = false;
-
+        
+        public int AquariumId { get; set; }
         public Aquarium Aquarium { get; set; }
     }
 }
