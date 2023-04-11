d7fc5b375e9e37b771912df934bd1cebbdebe972
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2023-04-11 10:37:57 +0200

    Basiq mqtt implementation

--- API/Services/MqttClientService.cs
+++ API/Services/MqttClientService.cs
@@ -1,21 +1,27 @@
 ﻿using System.Text;
+using System.Text.Json;
+using Aqua_Sharp_Backend.Interfaces;
+using Models.ViewModels.Measurement;
 using MQTTnet;
 using MQTTnet.Client;
+using MQTTnet.Protocol;
 
 namespace Aqua_Sharp_Backend.Services;
 
-public class MqttClientService: BackgroundService
+public sealed class MqttClientService: BackgroundService
 {
     private readonly IMqttClient _client;
     private readonly MqttClientOptions _clientOptions;
     private readonly MqttClientSubscribeOptions _subscriptionOptions;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
 
-    public MqttClientService(IConfiguration configuration)
+    public MqttClientService(IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
     {
+        _serviceScopeFactory = serviceScopeFactory;
+        
         var factory = new MqttFactory();
         _client = factory.CreateMqttClient();
-
-        _clientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+        _clientOptions = new MqttClientOptionsBuilder().WithTcpServer(configuration.GetValue<string>("Mqtt:Address")).Build();
         _subscriptionOptions = factory.CreateSubscribeOptionsBuilder().WithTopicFilter(f =>
         {
             f.WithTopic("measurement/#");
@@ -29,7 +35,6 @@ public class MqttClientService: BackgroundService
     {
         await _client.ConnectAsync(_clientOptions, CancellationToken.None);
         await _client.SubscribeAsync(_subscriptionOptions, CancellationToken.None);
-     
     }
     
     public override async Task StopAsync(CancellationToken cancellationToken)
@@ -43,19 +48,41 @@ public class MqttClientService: BackgroundService
         Dispose(true);
     }
 
-    protected virtual void Dispose(bool disposing)
+    private void Dispose(bool disposing)
     {
-        if(disposing)
-        {
-            _client.Dispose();
-            base.Dispose();
-        }
+        if (!disposing) return;
+        
+        _client.Dispose();
+        base.Dispose();
     }
 
     private async Task HandleMessageAsync(MqttApplicationMessageReceivedEventArgs e)
     {
         var payload=Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-    
+        var measurementViewModel = JsonSerializer.Deserialize<CreateMeasurementViewModel>(payload);
+        
+        if(measurementViewModel is null) 
+            return;
+
+        using var scope = _serviceScopeFactory.CreateScope(); 
+        var measurementService = scope.ServiceProvider.GetService<IMeasurementService>();
+        
+        if(measurementService is null)
+            return;
+        
+        var res = await measurementService.Create(measurementViewModel);
+        await PublishVerifiedMeasurementAsync(res);
+    }
+
+    private async Task PublishVerifiedMeasurementAsync(Measurement measurement)
+    {
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("verified/measurement/" + measurement.AquariumId)
+            .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtMostOnce)
+            .WithPayload(JsonSerializer.Serialize(measurement))
+            .Build();
+        
+        await _client.PublishAsync(message);
     }
 
 }
