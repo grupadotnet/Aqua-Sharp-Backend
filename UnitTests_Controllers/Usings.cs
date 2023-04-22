714372af34a0499bd99caad97bc6848cf530037f
Bartosz Bizoń <bartosz.bizon17@gmai.com>
2023-04-22 12:36:41 +0200

    Made this project only for controller tests

---
+++ UnitTests_Controllers/Usings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using Moq;
+global using Microsoft.AspNetCore.Mvc;
+global using Aqua_Sharp_Backend.Interfaces;
+global using Aqua_Sharp_Backend.Controllers;
+global using Models.Entities;
+global using Models.ViewModels;
+global using Aqua_Sharp_Backend.Profiles;
