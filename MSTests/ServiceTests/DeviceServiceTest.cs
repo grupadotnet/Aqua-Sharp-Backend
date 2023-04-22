34c205f14796d3241c28f5c73353b2c00aada186
Bartosz Bizoń <bartosz.bizon17@gmai.com>
2023-04-22 12:40:02 +0200

    Created tests base and basic test for DeviceService

---
+++ MSTests/ServiceTests/DeviceServiceTest.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+using UnitTests.Base;
+
+namespace UnitTests.ServiceTests
+{
+    [TestClass]
+    public sealed class DeviceServiceTest : ServiceTestBase
+    {
+        #region Properties
+        
+        private static readonly IAquariumService AquariumService = 
+            new AquariumService(MockContext.Object, Mapper);
+        
+        private static readonly IDeviceService DeviceService = 
+            new DeviceService(MockContext.Object, Mapper, AquariumService);
+
+        #endregion
+
+        [TestMethod]
+        public async Task AddTest()
+        {
+            var createDeviceViewModel = new CreateDeviceViewModel()
+            {
+                MeasurementFrequency = 1,
+                AquariumId = 1
+            };
+            
+            await DeviceService.Add(createDeviceViewModel);
+            
+                // var mockSetDevice = new Mock<DbSet<Device>>();
+                // MockContext.Setup(m => m.Devices).Returns(mockSetDevice.Object);
+            
+            MockSetDevice.Verify(m => m.Add(It.IsAny<Device>()), Times.Once());
+            MockContext.Verify(m => m.SaveChanges(), Times.Once());
+            
+            // --------------
+            
+            // var mockSet = new Mock<DbSet<Device>>();
+            //
+            // var mockContext = new Mock<Context>();
+            // mockContext.Setup(m => m.Devices).Returns(mockSet.Object);
+            //
+            // var service = new DeviceService(mockContext.Object, Mapper, AquariumService);
+            // await service.Add(createDeviceViewModel);
+            //
+            // mockSet.Verify(m => m.Add(It.IsAny<Device>()), Times.Once());
+            // mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+    }
+}
+
---
+++ MSTests/Usings.cs
@@ -0,0 +1,11 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Moq;
+global using Microsoft.EntityFrameworkCore;
+global using Models.Entities;
+global using Aqua_Sharp_Backend.Contexts;
+global using Aqua_Sharp_Backend.Services;
+global using Aqua_Sharp_Backend.Interfaces;
+global using Aqua_Sharp_Backend.Profiles;
+global using Models.ViewModels.Device;
+global using Microsoft.AspNetCore.Mvc;
+
