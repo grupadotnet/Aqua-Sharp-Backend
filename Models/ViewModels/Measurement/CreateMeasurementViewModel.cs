4b9dcea24834c7956b0cefca26cdccbda2c056ed
Szczepan Wikar <60351575+SzczepanWikar@users.noreply.github.com>
2023-04-26 19:44:45 +0200

    Update CreateMeasurementViewModel.cs

--- Models/ViewModels/Measurement/CreateMeasurementViewModel.cs
+++ Models/ViewModels/Measurement/CreateMeasurementViewModel.cs
@@ -4,6 +4,7 @@ public class CreateMeasurementViewModel
 {
     public DateTime Time { get; set; }
     public float Temperature { get; set; }
+    public float Ph { get; set; }
     public uint TDS { get; set; }
     public bool LightOn { get; set; }
     public int AquariumId { get; set; }
