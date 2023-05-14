d5e69a8d635ebae0a327846c6e84e35af8636d13
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2023-05-14 20:13:31 +0200

    HOT FIX: Add Ph property in Measurement

--- Models/Entities/Measurement.cs
+++ Models/Entities/Measurement.cs
@@ -5,6 +5,7 @@ namespace Models.Entities
         public int MeasurementId { get; set; }
         public DateTime Time { get; set; }
         public float Temperature { get; set; }
+        public float Ph { get; set; }
         public uint TDS { get; set; }
         public bool LightOn { get; set; }
         
