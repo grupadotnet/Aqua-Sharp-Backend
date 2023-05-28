2a6193a1806b6dcdb50b9fa6cd4b192bdfc948b3
Dawid Wilgucki <dawid123wilgucki@wp.pl>
2023-05-29 00:11:43 +0200

    Removed unnecessary comments

--- API/AutoMapperProfiles/AquariumMapper.cs
+++ API/AutoMapperProfiles/AquariumMapper.cs
@@ -9,16 +9,6 @@ namespace Aqua_Sharp_Backend.MapperProfiles
         public AquariumMapper()
         {
             CreateMap<EditAquariumViewModel, Aquarium>().ReverseMap();
-            //.ForMember(dest => dest.Temperature, opt => opt.MapFrom(src => src.Temperature))
-            //.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            //.ForMember(dest => dest.Sunset, opt => opt.MapFrom(src => src.Sunset))
-            //.ForMember(dest => dest.Dawn, opt => opt.MapFrom(src => src.Dawn))
-            //.ForMember(dest => dest.PH, opt => opt.MapFrom(src => src.PH))
-            //.ForMember(dest => dest.Width, opt => opt.Ignore())
-            //.ForMember(dest => dest.Length, opt => opt.Ignore())
-            //.ForMember(dest => dest.Height, opt => opt.Ignore())
-            //.ForMember(dest => dest.AquariumId, opt => opt.Ignore());
-
 
         }
     }
