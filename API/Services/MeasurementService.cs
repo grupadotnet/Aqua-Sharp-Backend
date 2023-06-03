905fec3e81933c3b181c66220bc51bdbf43745cb
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2023-06-04 01:39:46 +0200

    order pagination hotfix

--- API/Services/MeasurementService.cs
+++ API/Services/MeasurementService.cs
@@ -79,7 +79,6 @@ namespace Aqua_Sharp_Backend.Services
                 .Where(e => e.AquariumId == viewModel.AquariumId)
                 .Skip(viewModel.StartFrom-1)
                 .Take(PageSize)
-                .OrderByDescending(e => e.MeasurementId)
                 .ToListAsync();
 
             return res;
