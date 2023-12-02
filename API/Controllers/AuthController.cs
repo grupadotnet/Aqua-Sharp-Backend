cd30d3daefbd8bc6046bfdd77ceb4a7727e937e9
Przem0s84 <przemyslaw.wacek202@gmail.com>
2023-12-02 22:43:42 +0100

    AuthController change

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Aqua_Sharp_Backend.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.ViewModels.Config;
+
+namespace Aqua_Sharp_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfigService _configService;
+
+        public AuthController(IConfigService configService)
+        {
+            _configService = configService; 
+        }
+        [HttpPost]
+        [Route("login")]
+        public ActionResult Login([FromBody] LoginViewModel dto)
+        {
+            string token = _configService.GenerateJwt(dto);
+            return Ok(token);
+
+        }
+    }
+}
