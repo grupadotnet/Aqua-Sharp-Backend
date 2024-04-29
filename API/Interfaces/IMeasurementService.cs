4314e565005cc98d76dd4bccfba7c0adae83cf0e
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2024-04-29 21:00:28 +0200

    HOT FIX: IMeasurementService

--- API/Interfaces/IMeasurementService.cs
+++ API/Interfaces/IMeasurementService.cs
@@ -9,6 +9,5 @@ namespace Aqua_Sharp_Backend.Interfaces
         Task<List<Measurement>> Get(GetMeasurementsPaginationViewModel paginationViewModel);
         Task<List<Measurement>> Get(GetMeasurementsStartFromViewModel viewModel);
         Task Delete(int id);
-        Task SendMes(int userID);
     }
 }
